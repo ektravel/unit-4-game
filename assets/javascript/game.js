522a5404a849de7999915f11241e975e504571d7
ektravel <38017980+ektravel@users.noreply.github.com>
2018-04-23 00:35:51 -0500

    committing initial .js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,3 +1,15 @@
+var totalScore = 0;
+var randomNumber = randomNumberGenerator(19, 10);
+var wins = 0;
+var losses = 0;
+var sushiValue = randomNumberGenerator(1, 12);
+function randomNumberGenerator(min,max) {
+    return Math.floor(Math.random()*(max-min+1)+min);
+}
+
 $(document).ready(function(){
+    $("button").on("click", function() {
+        $("#randomNumber").text(randomNumber);
+    });
 
 });
